04b3f090a961ca36a9703d3edfdfad097be4cda0
MCmoderSD <mail@mcmodersd.de>
2026-01-27 23:15:54 +0100

    refactor Files and Objects classes for improved readability and structure

--- src/test/java/Files.java
+++ src/test/java/Files.java
@@ -3,45 +3,42 @@ import de.MCmoderSD.tools.GZIP;
 import java.io.File;
 import java.io.IOException;
 
-@SuppressWarnings("ALL")
-public class Files {
+// Input files for testing
+File input25 = new File("storage/input/25-Mio.txt");
+File input50 = new File("storage/input/50-Mio.txt");
+File input100 = new File("storage/input/100-Mio.txt");
 
-    // Input files for testing
-    private static final File input25 = new File("storage/input/25-Mio.txt");
-    private static final File input50 = new File("storage/input/50-Mio.txt");
-    private static final File input100 = new File("storage/input/100-Mio.txt");
+// Example usage of GZIP compression and decompression for files
+@SuppressWarnings("ResultOfMethodCallIgnored")
+void main() throws IOException {
 
-    // Example usage of GZIP compression and decompression for files
-    public static void main(String[] args) throws IOException {
+    // Output files for compressed data
+    File output25 = new File("storage/output/25-Mio.txt.gz");
+    File output50 = new File("storage/output/50-Mio.txt.gz");
+    File output100 = new File("storage/output/100-Mio.txt.gz");
 
-        // Output files for compressed data
-        File output25 = new File("storage/output/25-Mio.txt.gz");
-        File output50 = new File("storage/output/50-Mio.txt.gz");
-        File output100 = new File("storage/output/100-Mio.txt.gz");
+    // Create output files if they do not exist
+    if (!output25.exists()) output25.createNewFile();
+    if (!output50.exists()) output50.createNewFile();
+    if (!output100.exists()) output100.createNewFile();
 
-        // Create output files if they do not exist
-        if (!output25.exists()) output25.createNewFile();
-        if (!output50.exists()) output50.createNewFile();
-        if (!output100.exists()) output100.createNewFile();
+    // Compress files
+    GZIP.deflate(input25, output25);
+    GZIP.deflate(input50, output50);
+    GZIP.deflate(input100, output100);
 
-        // Compress files
-        GZIP.deflate(input25, output25);
-        GZIP.deflate(input50, output50);
-        GZIP.deflate(input100, output100);
+    // Decompress files
+    byte[] decompressed25 = GZIP.inflate(output25);
+    byte[] decompressed50 = GZIP.inflate(output50);
+    byte[] decompressed100 = GZIP.inflate(output100);
 
-        // Decompress files
-        byte[] decompressed25 = GZIP.inflate(output25);
-        byte[] decompressed50 = GZIP.inflate(output50);
-        byte[] decompressed100 = GZIP.inflate(output100);
+    // Print sizes for verification
+    IO.println("Decompressed 25-Mio size: " + decompressed25.length + " bytes");
+    IO.println("Decompressed 50-Mio size: " + decompressed50.length + " bytes");
+    IO.println("Decompressed 100-Mio size: " + decompressed100.length + " bytes");
 
-        // Print sizes for verification
-        System.out.println("Decompressed 25-Mio size: " + decompressed25.length + " bytes");
-        System.out.println("Decompressed 50-Mio size: " + decompressed50.length + " bytes");
-        System.out.println("Decompressed 100-Mio size: " + decompressed100.length + " bytes");
-
-        // Print Compression Ratios
-        System.out.println("Compression Ratio 25-Mio: " + (input25.length() / (double) output25.length()) + ":1");
-        System.out.println("Compression Ratio 50-Mio: " + (input50.length() / (double) output50.length()) + ":1");
-        System.out.println("Compression Ratio 100-Mio: " + (input100.length() / (double) output100.length()) + ":1");
-    }
+    // Print Compression Ratios
+    IO.println("Compression Ratio 25-Mio: " + (input25.length() / (double) output25.length()) + ":1");
+    IO.println("Compression Ratio 50-Mio: " + (input50.length() / (double) output50.length()) + ":1");
+    IO.println("Compression Ratio 100-Mio: " + (input100.length() / (double) output100.length()) + ":1");
 }
--- src/test/java/Objects.java
+++ src/test/java/Objects.java
@@ -5,46 +5,43 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.io.IOException;
 
-@SuppressWarnings("ALL")
-public class Objects {
 
-    public record Vector(float... coordinates) implements Serializable {
+record Vector(float... coordinates) implements Serializable {
 
-        public Vector {
-            if (coordinates.length == 0) throw new IllegalArgumentException("Coordinates cannot be empty");
-        }
-
-        public byte[] serialize() throws IOException {
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
-            objectOutputStream.writeObject(this);
-            objectOutputStream.flush();
-            return byteArrayOutputStream.toByteArray();
-        }
+    Vector {
+        if (coordinates.length == 0) throw new IllegalArgumentException("Coordinates cannot be empty");
     }
 
-    // Example usage of GZIP compression and decompression for objects
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-
-        // Create an n dimensional Vector with random coordinates
-        int n = 8192;
-        float[] coordinates = new float[n];
-        for (var i = 0; i < n; i++) coordinates[i] = Math.round(Math.random());
-        Vector vector = new Vector(coordinates);
-
-        // Serialize the vector
-        byte[] serializedVector = vector.serialize();
-        System.out.println("Serialized Vector size: " + serializedVector.length + " bytes");
-
-        // Compress the serialized vector
-        byte[] compressedVector = GZIP.deflateObject(vector);
-        System.out.println("Compressed Vector size: " + compressedVector.length + " bytes");
-
-        // Decompress the vector
-        Vector decompressedVector = (Vector) GZIP.inflateObject(compressedVector);
-        System.out.println("Decompressed Vector size: " + decompressedVector.serialize().length + " bytes");
-
-        // Print Compression Ratio
-        System.out.println("Compression Ratio: " + (serializedVector.length / (double) compressedVector.length) + ":1");
+    byte[] serialize() throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+        objectOutputStream.writeObject(this);
+        objectOutputStream.flush();
+        return byteArrayOutputStream.toByteArray();
     }
+}
+
+// Example usage of GZIP compression and decompression for objects
+void main() throws IOException, ClassNotFoundException {
+
+    // Create an n dimensional Vector with random coordinates
+    int n = 8192;
+    float[] coordinates = new float[n];
+    for (var i = 0; i < n; i++) coordinates[i] = Math.round(Math.random());
+    Vector vector = new Vector(coordinates);
+
+    // Serialize the vector
+    byte[] serializedVector = vector.serialize();
+    IO.println("Serialized Vector size: " + serializedVector.length + " bytes");
+
+    // Compress the serialized vector
+    byte[] compressedVector = GZIP.deflateObject(vector);
+    IO.println("Compressed Vector size: " + compressedVector.length + " bytes");
+
+    // Decompress the vector
+    Vector decompressedVector = (Vector) GZIP.inflateObject(compressedVector);
+    IO.println("Decompressed Vector size: " + decompressedVector.serialize().length + " bytes");
+
+    // Print Compression Ratio
+    IO.println("Compression Ratio: " + (serializedVector.length / (double) compressedVector.length) + ":1");
 }
